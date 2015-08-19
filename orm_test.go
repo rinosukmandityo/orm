bdf435693103b915b5b21d4876a7702d9a585e3b
juragan360 <ariefda@hotmail.com>
2015-08-19 08:58:20 +0700

    update test

--- orm_test.go
+++ orm_test.go
@@ -1,11 +1,11 @@
 package orm
 
 import (
-	_ "github.com/eaciit/database/base"
-	tk "github.com/eaciit/toolkit"
 	"fmt"
+	_ "github.com/eaciit/database/base"
 	"github.com/eaciit/database/mongodb"
 	_ "github.com/eaciit/parallel"
+	tk "github.com/eaciit/toolkit"
 	"runtime"
 	"strconv"
 	_ "sync"
@@ -14,7 +14,7 @@ import (
 )
 
 type UserModel struct {
-	ModelBase `bson:"-"`
+	ModelBase `bson:"-",json:"-"`
 	Id        string `bson:"_id",json:"_id"`
 	FullName  string `bson:"fullname"`
 	Email     string
@@ -34,7 +34,7 @@ func init() {
 }
 
 func prepareContext() (*DataContext, error) {
-	conn := mongodb.NewConnection("localhost:27888", "", "", "ectest")
+	conn := mongodb.NewConnection("localhost:27123", "", "", "ectest")
 	if eConnect := conn.Connect(); eConnect != nil {
 		return nil, eConnect
 	}
@@ -56,12 +56,12 @@ func TestLoadAll(t *testing.T) {
 	}
 	defer ctx.Close()
 
-	fmt.Println("Test Load All")	
-	c := ctx.Find(new(UserModel),tk.M{
-		"where":nil,
-		"order":[]string{"_id"},
-		"take":0,
-		"limit":0,
+	fmt.Println("Test Load All")
+	c := ctx.Find(new(UserModel), tk.M{
+		"where": nil,
+		"order": []string{"_id"},
+		"take":  0,
+		"limit": 0,
 	})
 	defer c.Close()
 	if c == nil {
@@ -73,9 +73,9 @@ func TestLoadAll(t *testing.T) {
 		if count > 0 {
 			_, e = c.Fetch(&user)
 		}
-		if e==nil {
+		if e == nil {
 			fmt.Printf("OK...")
-			fmt.Printf("Record(s) found: %d\nSample of first record%v \n", count, tk.IfEq(count,0,nil,user))
+			fmt.Printf("Record(s) found: %d\nSample of first record%v \n", count, tk.IfEq(count, 0, nil, user))
 			fmt.Println("")
 		} else {
 			fmt.Println("NOK")
@@ -88,9 +88,9 @@ func TestInsert(t *testing.T) {
 	//t.Skip()
 	ctx, _ := prepareContext()
 	defer ctx.Close()
-	
+
 	ctx.DeleteMany(new(UserModel), nil)
-	
+
 	t0 := time.Now()
 	count := 10000
 	for i := 0; i < count; i++ {
@@ -103,7 +103,7 @@ func TestInsert(t *testing.T) {
 		u.Enable = 1
 		e = ctx.Insert(u)
 		if e != nil {
-			t.Errorf("Error Load %d: %s",i, e.Error())
+			t.Errorf("Error Load %d: %s", i, e.Error())
 			return
 		} else {
 			fmt.Println("OK")
@@ -116,7 +116,7 @@ func TestUpdate(t *testing.T) {
 	t.Skip()
 	ctx, _ := prepareContext()
 	defer ctx.Close()
-	
+
 	t0 := time.Now()
 	count := 100
 	for i := 0; i < count; i++ {
@@ -129,7 +129,7 @@ func TestUpdate(t *testing.T) {
 		u.Enable = 1
 		e = ctx.Save(u)
 		if e != nil {
-			t.Errorf("Error Load %d: %s",i, e.Error())
+			t.Errorf("Error Load %d: %s", i, e.Error())
 			return
 		} else {
 			fmt.Println("OK")
@@ -147,7 +147,7 @@ func TestDelete(t *testing.T) {
 	}
 	defer ctx.Close()
 	u := new(UserModel)
-	e = ctx.GetById(u,"user01")
+	e = ctx.GetById(u, "user01")
 	if e == nil {
 		fmt.Printf("Will Delete UserModel: %v \n", u)
 		e = ctx.Delete(u)
