533b20a928d76e397c005fba035a02733b6cdb39
Arief Darmawan <ariefda@hotmail.com>
2016-05-02 05:28:36 +0700

    various

--- cli/gen.go
+++ cli/gen.go
@@ -4,6 +4,10 @@ import (
     "errors"
     "strings"
     "github.com/eaciit/toolkit"
+    "path/filepath"
+    "os"
+    "bufio"
+    //"io/ioutil"
 )
 
 type StructModel struct{
@@ -24,7 +28,42 @@ func (sm *StructModel) Write(path string)error{
     if sm.PkgName=="" || sm.Name=="" {
         return toolkit.Errorf("Both package name and struct name should be defined")
     }
-    return toolkit.Errorf("Fail to write %s.%s : method Write is not yet implemented", sm.PkgName, sm.Name)
+    //return toolkit.Errorf("Fail to write %s.%s : method Write is not yet implemented", sm.PkgName, sm.Name)
+    
+    filename := filepath.Join(path, strings.ToLower(sm.Name) + ".go")
+    //currentCode := ""
+    f, e := os.Open(filename)
+    if e==nil {
+        //bcurrent, _ := ioutil.ReadAll(f)
+        //currentCode = string(bcurrent)
+        os.Remove(filename)
+    }   
+    
+    f, e = os.Create(filename)
+    if e!=nil {
+        return toolkit.Errorf("Faild to write %s.%s: %s", sm.PkgName, sm.Name, e.Error())
+    }
+    defer f.Close()
+    
+    txts := []string{}
+    txts = append(txts,"package " + sm.PkgName)
+    txts = append(txts,"/*** OrmGen Auto Generate Code - Start ***/")
+    txts = append(txts,"struct " + sm.Name + "{")
+    txts = append(txts,"}")
+    txts = append(txts,"/*** OrmGen Auto Generate Code - End ***/")
+     
+    b := bufio.NewWriter(f)
+    for _, txt := range txts{
+        b.WriteString(txt + "\n")
+    }
+    
+    e = b.Flush()
+    if e!=nil {
+        return toolkit.Errorf("Faild to write %s.%s: %s", sm.PkgName, sm.Name, e.Error())
+    }
+    
+    toolkit.RunCommand("/bin/sh", "-c", "gofmt -w "+filename)
+    return nil
 }
 
 func getPackage(txt string)(string,error){
